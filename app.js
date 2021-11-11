39847667856cc682607205a90142450371e1148b
ferrerama <77178939+ferrerama@users.noreply.github.com>
2021-11-11 15:12:31 -0600

    Add files via upload
    Prueba

---
+++ app.js
@@ -0,0 +1,12 @@
+var lim_edad = 18;
+var edad_act= 17;
+
+if(lim_edad<edad_act){
+    console.log("Pase adelante");
+}
+else if(lim_edad==edad_act){
+    console.log("Casi casi, pero pase.");
+}
+else{
+    console.log("Ud es menor de edad, regrese a casa");
+}
